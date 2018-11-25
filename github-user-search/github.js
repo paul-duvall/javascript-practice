8cab25f8ad340da3c24d641df295455404170cb7
Paul Duvall <duvallpj@gmail.com>
2018-11-25 09:43:00 +0000

    Updated to display five most recent GitHub repos

--- github-user-search/github.js
+++ github-user-search/github.js
@@ -2,18 +2,27 @@ class Github {
   constructor() {
     this.client_id = '9be118bd0629f123304a';
     this.client_secret = 'ba65cda75969028fc7b3d6e18c5363f4a22999ea';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
-    // Define variable containing API
+    // Define variable containing API for user
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    // Define variable containing API for user's repos
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
     // Define variable containing the data from the API
     const profile = await profileResponse.json();
 
+    // Define variable containing the data from the API for repos
+    const repos = await repoResponse.json();
+
     // Return an object containing the data (note that only 'profile' is needed - will set key to 'profile' and the value to the content of the profile variable)
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
--- github-user-search/ui.js
+++ github-user-search/ui.js
@@ -31,6 +31,32 @@ class UI {
     `;
   }
 
+  // Show user repos
+  showRepos(repos) {
+    let output = '';
+
+    repos.forEach(function(repo) {
+      output += `
+        <div class="card card-body mb-2">
+          <div class="row">
+            <div class="col-md-6">
+              <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+            </div>
+            <div class="col-md-6">
+              <span class="badge badge-primary">Stars: ${repo.stargazer_count}</span>
+              <span class="badge badge-secondary">Watchers: ${repo.watchers}</span>
+              <span class="badge badge-success">Forks: ${repo.forks_count}</span>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output;
+
+  }
+
   // Show alert message if there is no user profile associated with input
   showAlert(message, className) {
     // Clear any currently displayed alert
